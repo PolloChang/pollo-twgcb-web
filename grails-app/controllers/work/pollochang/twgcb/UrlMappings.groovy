0b7c5e358dddeb28004c6f2ae3da1c227c6563f3
PolloChang <james89926282@gmail.com>
2024-04-14 14:46:11 +0800

    fix(index): 修正預設首頁

--- grails-app/controllers/work/pollochang/twgcb/UrlMappings.groovy
+++ grails-app/controllers/work/pollochang/twgcb/UrlMappings.groovy
@@ -8,7 +8,7 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
+        "/"(controller: "twGcb", action: "index")
         "500"(view:'/error')
         "404"(view:'/notFound')
 
